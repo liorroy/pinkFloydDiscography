f5c5377e05d1be90765045c39e0707c25c0dca52
Lior Roytburd <43315313+liorroy@users.noreply.github.com>
2024-06-21 16:41:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os  # to interacte with the operating system (walking through directories to parse the YAML files)
+import re  # for regular expressions - not used right now
+import sys  # for command-line arguments
+
+
+def load_db(directory):
+    discog = {}
+    file_path = os.path.join()
+    with open(file_path, 'r') as file:
+        data = file.readlines(file_path)
+            if x is x:
+
+
+
+    print(data)
+    return data
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    directory = "/Users/Study/Downloads/" # default
+    load_db(directory)
